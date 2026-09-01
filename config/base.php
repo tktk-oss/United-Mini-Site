d55b7f9922aa418c52d92b06cb982d6003165712
unknown <mefoing@gmail.com>
2026-09-01 17:35:55 +0200

    Suppression de dossiers inutiles 2

--- config/base.php
+++
@@ -1,9 +0,0 @@
-<?php
-// Chemin absolu sur le serveur (pour les inclusions de fichiers PHP)
-define('ROOT_PATH', dirname(__DIR__) . '/');
-define('TEMPLATE_PATH', ROOT_PATH . 'templates/');
-
-// URL racine du projet (pour les liens HTML, CSS, JS et navigation)
-// Remplacez par votre URL locale (ex: http://localhost/mon-projet-php/public/)
-define('URL_ROOT', '/mon-projet-php/public/'); 
-?>
